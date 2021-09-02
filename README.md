810bb5aa0a9eb4ae8708889bcbb4aea660a5333a
Parakh M. Gupta <parakhm95@gmail.com>
2021-09-03 00:53:26 +0530

    Kalman filter implemented
    Works but not too well
    Issues are listed in readme.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # usv_estiplan
 linking libraries ROS https://answers.ros.org/question/239690/how-to-include-a-library-in-cmakelists/
+
+> re-existing modes should not be re-initialized with bad phase
+> Frequencies with very low amplitude need to be discarded to prevent noise
+> Low amplitude can be gated using a ratio of the highest amplitude
--- src/wave_prediction.cpp
+++ src/wave_prediction.cpp
@@ -4,120 +4,129 @@
 #include "sensor_msgs/Imu.h"
 #include "std_msgs/Float64.h"
 #include "usv_estiplan/Fftoutput.h"
+#include <Eigen/Dense>
 #include <cmath>
 #include <fstream>
 #include <iostream>
 #include <queue>
 #include <sstream>
+#include <unsupported/Eigen/MatrixFunctions>
 
 using namespace std;
 
 #define REAL 0
 #define IMAG 1
 
-// const int max_queue = 10000;
-// const int samp_freq = 83;
-const int wave_components = 20;
-// queue<float> x_queue;
-// queue<float> y_queue;
+const int WAVE_COMPONENTS = 20;
+const float SAMPLE_TIME = 0.01;
 uint64_t iter_global = 0;
-// fftw_complex in_x[N];
-// fftw_complex in_y[N];
-// sensor_msgs::Imu imu_data{};
-// geometry_msgs::Vector3 fft_data{};
 usv_estiplan::Fftoutput fft_msg{};
 double msg_time;
 std_msgs::Float64 wave_msg;
-// void queue_things(queue<float> &queue_in,float push_in){
-// 	while(queue_in.size() > max_queue){
-// 		queue_in.pop();
-// 	}
-// 		queue_in.push(push_in);
-// }
+Eigen::VectorXd w_k_hat(1);
+Eigen::VectorXd w_k(1);
+Eigen::Vector2d x_i;                                       // X(i,0) single mode
+Eigen::VectorXd x_t(2 * (WAVE_COMPONENTS + 1), 1);         // X(t_0) mode vector
+Eigen::VectorXd l_k(2 * (WAVE_COMPONENTS + 1), 1);         // L_k Kalman gain
+Eigen::VectorXd x_predicted(2 * (WAVE_COMPONENTS + 1), 1); // X_k+1 predicted
+// A(t_0)
+Eigen::Matrix<double, 2 * (WAVE_COMPONENTS + 1), 2 * (WAVE_COMPONENTS + 1)>
+    a_t = Eigen::MatrixXd::Identity(2 * (WAVE_COMPONENTS + 1),
+                                    2 * (WAVE_COMPONENTS + 1));
+// Phi_0
+Eigen::MatrixXd phi(2 * (WAVE_COMPONENTS + 1), 2 * (WAVE_COMPONENTS + 1));
+// P_k+1 predicted
+Eigen::Matrix<double, 2 * (WAVE_COMPONENTS + 1), 2 * (WAVE_COMPONENTS + 1)>
+    p_k_dash = Eigen::MatrixXd::Zero(2 * (WAVE_COMPONENTS + 1),
+                                     2 * (WAVE_COMPONENTS + 1));
+// P_k+1 actual
+Eigen::Matrix<double, 2 * (WAVE_COMPONENTS + 1), 2 * (WAVE_COMPONENTS + 1)>
+    p_k = Eigen::MatrixXd::Zero(2 * (WAVE_COMPONENTS + 1),
+                                2 * (WAVE_COMPONENTS + 1));
 
-void fftCallback(usv_estiplan::Fftoutput in_msg) {
+Eigen::MatrixXd c_t(1, 2 * (WAVE_COMPONENTS + 1)); // C(t_0)
+Eigen::Matrix2d a_i;                               // A_i(t_0)
+Eigen::VectorXd r(1);
+Eigen::Matrix<double, 2 * (WAVE_COMPONENTS + 1), 2 * (WAVE_COMPONENTS + 1)>
+    q_dash;
+Eigen::Matrix<double, 2 * (WAVE_COMPONENTS + 1), 2 * (WAVE_COMPONENTS + 1)> q =
+    Eigen::MatrixXd::Identity(2 * (WAVE_COMPONENTS + 1),
+                              2 * (WAVE_COMPONENTS + 1)) *
+    SAMPLE_TIME;
+bool first_start = true;
+
+void ImuCallback(sensor_msgs::Imu imu_data) {
+  w_k(0) = imu_data.angular_velocity.x;
+}
+
+void FftCallback(usv_estiplan::Fftoutput in_msg) {
   fft_msg = in_msg;
+  first_start = false;
+  for (size_t i = 0; i < WAVE_COMPONENTS; i++) {
+    // changing A matrices
+    a_i(1) = -pow(2 * M_PI * fft_msg.frequency[i], 2);
+    a_t.block(2 * i, 2 * i, 2, 2) = a_i;
+    // changing X matrices
+    x_i(0) = fft_msg.amplitude[i] * sin(fft_msg.phase[i]);
+    x_i(1) = 2 * M_PI * fft_msg.amplitude[i] * fft_msg.frequency[i] *
+             cos(fft_msg.phase[i]);
+    x_t.block(2 * i, 0, 2, 1) = x_i;
+  }
+  // updating Phi
+  phi = (a_t * SAMPLE_TIME).exp();
   msg_time = ros::Time::now().toNSec();
 }
-// void convert_que_to_array(queue<float> dupl,fftw_complex *fftw_in){
-// 	int fft_size = dupl.size(); // using dupl.size() multiple times was
-// causing for loop to use garbage i values. 	for(int i=0;i < fft_size;i++){
-// 		fftw_in[i][REAL] = dupl.front();
-// 		fftw_in[i][IMAG] = 0.0;
-// 		dupl.pop();
-// 	}
-
-// }
 
 int main(int argc, char **argv) {
 
+  r(0) = 0.01;
+  x_t(2 * WAVE_COMPONENTS) = 0.0;
+  x_t(2 * WAVE_COMPONENTS + 1) = 0.0;
+  a_i(0) = 0.0;
+  a_i(2) = 1.0;
+  a_i(3) = 0.0;
+  for (size_t i = 0; i < WAVE_COMPONENTS + 1; i++) {
+    c_t << 1.0, 0.0;
+  }
   ros::init(argc, argv, "wave_prediction");
   ros::NodeHandle estiplan;
-  ros::Subscriber sub = estiplan.subscribe("/fft_output", 1000, fftCallback);
+  ros::Subscriber sub = estiplan.subscribe("/fft_output/", 1000, FftCallback);
+  ros::Subscriber sub_imu = estiplan.subscribe("/wamv/imu", 1000, ImuCallback);
   ros::Publisher wave_predictor =
       estiplan.advertise<std_msgs::Float64>("/wave_prediction", 1000);
-  // ros::Publisher fft_transform =
-  // estiplan.advertise<geometry_msgs::Vector3>("/fft_output/y", 1000);
-  ros::Rate loop_rate(100.0);
+  ros::Rate loop_rate(1 / SAMPLE_TIME);
   double wave_output;
   double time_elap = 0;
   while (ros::ok()) {
-    wave_output = 0;
-    for (size_t i = 0; i < wave_components; i++) {
-      time_elap = (ros::Time::now().toNSec() - msg_time) * 1e-9;
-      wave_output +=
-          fft_msg.amplitude[i] *
-          sin((fft_msg.frequency[i] * 2 * M_PI * time_elap) + fft_msg.phase[i]);
+    if (!first_start) {
+      x_predicted = phi * x_t;
+      q_dash = 0.5 * (phi * q * phi.transpose() + q) * SAMPLE_TIME;
+      p_k_dash = (phi * p_k_dash * phi.transpose() + q_dash).eval();
+      l_k = p_k_dash * c_t.transpose() *
+            (c_t * p_k_dash * c_t.transpose() + r).inverse();
+      w_k_hat = c_t * x_t;
+      x_predicted = (x_predicted + (l_k * (w_k - w_k_hat))).eval();
+      w_k_hat = c_t * x_predicted;
+      p_k = (Eigen::MatrixXd::Identity(2 * (WAVE_COMPONENTS + 1),
+                                       2 * (WAVE_COMPONENTS + 1)) -
+             (l_k * c_t)) *
+            p_k_dash;
+      x_t = x_predicted;
+      p_k_dash = p_k;
+      wave_output = 0;
+      for (size_t i = 0; i < WAVE_COMPONENTS; i++) {
+        time_elap = (ros::Time::now().toNSec() - msg_time) * 1e-9;
+        wave_output += fft_msg.amplitude[i] *
+                       sin((fft_msg.frequency[i] * 2 * M_PI * time_elap) +
+                           fft_msg.phase[i]);
+      }
+      wave_msg.data = w_k_hat(0);
+      wave_predictor.publish(wave_msg);
     }
-    wave_msg.data = wave_output;
-
-    // ofstream fft_output_capt;
-    // fft_output_capt.open("fft_data.csv");
-    // x_queue.push(0); // to prevent core dump
-    // 	// one fft block begin
-    // 	int fft_size = x_queue.size();
-    // 	fftw_complex in_fftw_x[fft_size] = {};
-    // 	fftw_complex out_fftw_x[fft_size] = {};
-    // 	convert_que_to_array(x_queue, in_fftw_x);
-    // 	fftw_plan plan =
-    // fftw_plan_dft_1d(fft_size,in_fftw_x,out_fftw_x,FFTW_FORWARD,
-    // FFTW_ESTIMATE); 	fftw_execute(plan); 	fftw_destroy_plan(plan);
-    // 	fftw_cleanup();
-    // one fft block end
-    // one fft block begin
-    // fft_size = y_queue.size();
-    // fftw_complex in_fftw_y[fft_size] = {};
-    // fftw_complex out_fftw_y[fft_size] = {};
-    // convert_que_to_array(y_queue, in_fftw_y);
-    // plan = fftw_plan_dft_1d(fft_size,in_fftw_y,out_fftw_y,FFTW_FORWARD,
-    // FFTW_ESTIMATE); fftw_execute(plan); fftw_destroy_plan(plan);
-    // fftw_cleanup();
-    // one fft block end
-
-    // double x_output[wave_components][3];
-    // double y_output[wave_components][3];
-
-    // cout << "FFT accuracy(Hz) : " << float(samp_freq/2.0f) / (fft_size/2.0f)
-    // << endl; int filler = 0; for (int i=0;i<fft_size/2;i++){ 	iter_global +=
-    // 1; 	fft_data.x = i * float(samp_freq/2) / (fft_size/2); 	fft_data.y
-    // = 2.0f*sqrt(pow(out_fftw_x[i][REAL],2) +
-    // pow(out_fftw_x[i][IMAG],2))/float(fft_size); 	fft_data.z =
-    // atan2(out_fftw_x[i][IMAG],out_fftw_x[i][REAL]); 	if(fft_data.y > 0.02 &&
-    // filler < 10){ 		fft_msg.frequency[filler] = fft_data.x;
-    // 		fft_msg.amplitude[filler] = fft_data.y;
-    // 		fft_msg.phase[filler] = fft_data.z;
-    // 		filler += 1;
-    // 	// fft_output_capt << iter_global << ";" << fft_data.x << ";" <<
-    // fft_data.y << ";" << fft_data.z <<'\n';
-    // 	}
-    // ros::Duration(0.01).sleep();
-
-    wave_predictor.publish(wave_msg);
     if (ros::isShuttingDown()) {
-      // fft_output_capt.close();
     }
-    ros::spinOnce();
     loop_rate.sleep();
+    ros::spinOnce();
   }
 
   return 0;
