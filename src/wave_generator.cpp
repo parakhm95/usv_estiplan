df438f1c486e8e39b6d52cf284c4d7dc59a73b95
Parakh M. Gupta <parakhm95@gmail.com>
2021-09-17 02:46:08 +0530

    A new wave generator for full-fledged testing

--- src/wave_generator.cpp
+++ src/wave_generator.cpp
@@ -1,4 +1,5 @@
 // #include <fftw3.h>
+#include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Vector3.h"
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
@@ -9,121 +10,91 @@
 #include <queue>
 #include <random>
 #include <sstream>
+#include <tf2/LinearMath/Quaternion.h>
 
 using namespace std;
 
 #define REAL 0
 #define IMAG 1
 
-// const int max_queue = 10000;
-// const int samp_freq = 83;
 const int wave_components = 10;
-const float CONSTANT_OFFSET = 0.2;
-// queue<float> x_queue;
-// queue<float> y_queue;
+const float CONSTANT_OFFSET_X = 13.2;
+const float CONSTANT_OFFSET_Y = 40.2;
+const float CONSTANT_OFFSET_Z = 9.2;
+double roll = 0.0;
+double pitch = 0.0;
+double yaw = 0.0;
 uint64_t iter_global = 0;
-// fftw_complex in_x[N];
-// fftw_complex in_y[N];
-sensor_msgs::Imu imu_msg{};
-// geometry_msgs::Vector3 fft_data{};
-// usv_estiplan::Fftoutput fft_msg{};
+string odom_topic_name;
+geometry_msgs::PoseStamped pose_msg{};
+geometry_msgs::Vector3 rpy_msg{};
 double msg_time;
-// std_msgs::Float64 wave_msg;
-double amplitude[wave_components] = {0.1,  0.2,  0.15, 0.7,  0.65,
-                                     0.35, 0.23, 0.5,  0.05, 0.85};
-double frequency[wave_components] = {0.1,  0.4,  0.25, 0.31, 0.05,
-                                     0.35, 0.13, 0.04, 0.48, 0.6};
-double phase[wave_components] = {0.1, 0.2, 0.3, 0.4, 0.5,
+double one[wave_components] = {0.1,  0.2,  0.15, 0.7,  0.65,
+                               0.35, 0.23, 0.5,  0.05, 0.85};
+double two[wave_components] = {0.1,  0.4,  0.25, 0.31, 0.05,
+                               0.35, 0.13, 0.04, 0.48, 0.6};
+double three[wave_components] = {0.1, 0.2, 0.3, 0.4, 0.5,
                                  0.6, 0.7, 0.8, 0.9, 1.0};
-// void queue_things(queue<float> &queue_in,float push_in){
-// 	while(queue_in.size() > max_queue){
-// 		queue_in.pop();
-// 	}
-// 		queue_in.push(push_in);
-// }
-
-// void fftCallback(usv_estiplan::Fftoutput in_msg)
-// {
-// 	fft_msg = in_msg;
-// 	msg_time = ros::Time::now().toNSec();
-// }
-// void convert_que_to_array(queue<float> dupl,fftw_complex *fftw_in){
-// 	int fft_size = dupl.size(); // using dupl.size() multiple times was
-// causing for loop to use garbage i values. 	for(int i=0;i < fft_size;i++){
-// 		fftw_in[i][REAL] = dupl.front();
-// 		fftw_in[i][IMAG] = 0.0;
-// 		dupl.pop();
-// 	}
-
-// }
 
 int main(int argc, char **argv) {
 
   ros::init(argc, argv, "wave_generator");
-  ros::NodeHandle estiplan;
-  // ros::Subscriber sub = estiplan.subscribe("/fft_output", 1000, fftCallback);
+  ros::NodeHandle estiplan("~");
+  if (!estiplan.getParam("/topics/odom", odom_topic_name)) {
+    ROS_ERROR("WAVE_GEN:odom_topic loading failed");
+    return -1;
+  }
   ros::Publisher wave_generator =
-      estiplan.advertise<sensor_msgs::Imu>("/wamv/imu", 1000);
-  // ros::Publisher fft_transform =
-  // estiplan.advertise<geometry_msgs::Vector3>("/fft_output/y", 1000);
+      estiplan.advertise<geometry_msgs::PoseStamped>(odom_topic_name, 1000);
+    ros::Publisher rpy_generator =
+      estiplan.advertise<geometry_msgs::Vector3>(odom_topic_name+"/rpy", 1000);
   ros::Rate loop_rate(100.0);
   double wave_output;
   double time_elap = 0;
   while (ros::ok()) {
-    wave_output = 0;
+    pose_msg.pose.position.x = 0.0;
+    pose_msg.pose.position.y = 0.0;
+    pose_msg.pose.position.z = 0.0;
+    roll = 0.0;
+    pitch = 0.0;
+    yaw = 0.0;
+    time_elap = ros::Time::now().toNSec() * 1e-9;
     for (size_t i = 0; i < wave_components; i++) {
-      time_elap = ros::Time::now().toNSec() * 1e-9;
-      wave_output +=
-          amplitude[i] * sin((frequency[i] * 2 * M_PI * time_elap) + phase[i]);
+      pose_msg.pose.position.x +=
+          one[i] * sin((two[i] * 2 * M_PI * time_elap) + three[i]);
+      pose_msg.pose.position.y +=
+          one[i] * sin((three[i] * 2 * M_PI * time_elap) + two[i]);
+      pose_msg.pose.position.z +=
+          two[i] * sin((one[i] * 2 * M_PI * time_elap) + three[i]);
+      roll += two[i] * sin((three[i] * 2 * M_PI * time_elap) + one[i]);
+      pitch += three[i] * sin((one[i] * 2 * M_PI * time_elap) + two[i]);
+      yaw += three[i] * sin((two[i] * 2 * M_PI * time_elap) + one[i]);
     }
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<double> dist(-0.3, 0.3);
-    imu_msg.angular_velocity.x = wave_output + dist(mt) + CONSTANT_OFFSET;
+    std::uniform_real_distribution<double> dist_small(-0.05, 0.05);
+    pose_msg.pose.position.x += dist(mt) + CONSTANT_OFFSET_X;
+    pose_msg.pose.position.y += dist(mt) + CONSTANT_OFFSET_Y;
+    pose_msg.pose.position.z += dist(mt) + CONSTANT_OFFSET_Z;
+    roll = roll/3 + dist_small(mt);
+    pitch = pitch/3 + dist_small(mt);
+    yaw = yaw/3 + dist_small(mt);
+    rpy_msg.x = roll;
+    rpy_msg.y = pitch;
+    rpy_msg.z = yaw;
+    tf2::Quaternion myQuaternion;
+    myQuaternion.setRPY(
+        roll, pitch,
+        yaw); // Create this quaternion from roll/pitch/yaw (in radians)
+        myQuaternion.normalize();
+    pose_msg.pose.orientation.x = myQuaternion[0];
+    pose_msg.pose.orientation.y = myQuaternion[1];
+    pose_msg.pose.orientation.z = myQuaternion[2];
+    pose_msg.pose.orientation.w = myQuaternion[3];
 
-    // ofstream fft_output_capt;
-    // fft_output_capt.open("fft_data.csv");
-    // x_queue.push(0); // to prevent core dump
-    // 	// one fft block begin
-    // 	int fft_size = x_queue.size();
-    // 	fftw_complex in_fftw_x[fft_size] = {};
-    // 	fftw_complex out_fftw_x[fft_size] = {};
-    // 	convert_que_to_array(x_queue, in_fftw_x);
-    // 	fftw_plan plan =
-    // fftw_plan_dft_1d(fft_size,in_fftw_x,out_fftw_x,FFTW_FORWARD,
-    // FFTW_ESTIMATE); 	fftw_execute(plan); 	fftw_destroy_plan(plan);
-    // 	fftw_cleanup();
-    // one fft block end
-    // one fft block begin
-    // fft_size = y_queue.size();
-    // fftw_complex in_fftw_y[fft_size] = {};
-    // fftw_complex out_fftw_y[fft_size] = {};
-    // convert_que_to_array(y_queue, in_fftw_y);
-    // plan = fftw_plan_dft_1d(fft_size,in_fftw_y,out_fftw_y,FFTW_FORWARD,
-    // FFTW_ESTIMATE); fftw_execute(plan); fftw_destroy_plan(plan);
-    // fftw_cleanup();
-    // one fft block end
-
-    // double x_output[wave_components][3];
-    // double y_output[wave_components][3];
-
-    // cout << "FFT accuracy(Hz) : " << float(samp_freq/2.0f) / (fft_size/2.0f)
-    // << endl; int filler = 0; for (int i=0;i<fft_size/2;i++){
-    // iter_global
-    // += 1; 	fft_data.x = i * float(samp_freq/2) / (fft_size/2);
-    // fft_data.y = 2.0f*sqrt(pow(out_fftw_x[i][REAL],2) +
-    // pow(out_fftw_x[i][IMAG],2))/float(fft_size); 	fft_data.z =
-    // atan2(out_fftw_x[i][IMAG],out_fftw_x[i][REAL]); 	if(fft_data.y > 0.02 &&
-    // filler < 10){ 		fft_msg.frequency[filler] = fft_data.x;
-    // 		fft_msg.amplitude[filler] = fft_data.y;
-    // 		fft_msg.phase[filler] = fft_data.z;
-    // 		filler += 1;
-    // 	// fft_output_capt << iter_global << ";" << fft_data.x << ";" <<
-    // fft_data.y << ";" << fft_data.z <<'\n';
-    // 	}
-    // ros::Duration(0.01).sleep();
-
-    wave_generator.publish(imu_msg);
+    wave_generator.publish(pose_msg);
+    rpy_generator.publish(rpy_msg);
     if (ros::isShuttingDown()) {
       // fft_output_capt.close();
     }
