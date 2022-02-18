0ecd6183a4ca3ce833bf929b11ba59c8b41e7560
parakh <drone@mrs.com>
2022-02-18 21:01:10 +0400

    MODULAR TREATMENT FOR APRIL_TAG_BRIDGE

--- scripts/april_tag_bridge.py
+++ scripts/april_tag_bridge.py
@@ -17,24 +17,25 @@ rpy_msg = Vector3()
 tf_buffer = tf2_ros.Buffer(rospy.Duration(100.0))  # tf buffer length
 
 # topics for subs and pubs
-tag_topic = "/uav1/rs_d435/color/base_detections"
 gazebo_model_name = "wamv"
 # bools and variables
 tag_publish_rate = 200.0
 elap_time =  0.0000
 publish_tag_detections = False
 random_noise = True
+tag_pub =  None
+tag_frame = None
 # freq, amp, phase
 
 wave_pub = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=1000)
-tag_pub = rospy.Publisher(tag_topic, PoseStamped, queue_size=1000)
 
 
 def callback(msg):
+    global tag_pub
     for i in range(len(msg.detections)):
         if(msg.detections[i].id[0] == 2):
             tag_msg.header.stamp = rospy.Time.now()
-            tag_msg.header.frame_id = 'uav1/gps_origin'
+            tag_msg.header.frame_id = tag_frame
             tag_msg.pose.position.x = msg.detections[i].pose.pose.pose.position.x
             tag_msg.pose.position.y = msg.detections[i].pose.pose.pose.position.y
             tag_msg.pose.position.z = msg.detections[i].pose.pose.pose.position.z
@@ -42,7 +43,7 @@ def callback(msg):
             tag_msg.pose.orientation.x = msg.detections[i].pose.pose.pose.orientation.x
             tag_msg.pose.orientation.y = msg.detections[i].pose.pose.pose.orientation.y
             tag_msg.pose.orientation.z = msg.detections[i].pose.pose.pose.orientation.z
-            transform = tf_buffer.lookup_transform('uav1/gps_origin',
+            transform = tf_buffer.lookup_transform(tag_msg.header.frame_id,
                                             # source frame:
                                             msg.header.frame_id,
                                             # get the tf at the time the pose was valid
@@ -54,9 +55,12 @@ def callback(msg):
             tag_pub.publish(tag_msg)
 
 def bridge():
+    global tag_pub, tag_frame
     rospy.init_node('april_tag_bridge', anonymous=True)
-    rospy.Subscriber("/uav1/rs_d435/color/tag_detections", AprilTagDetectionArray, callback)
+    rospy.Subscriber("~tag_detections_in", AprilTagDetectionArray, callback)
+    tag_pub = rospy.Publisher('~base_detections_out', PoseStamped, queue_size=1000)
     tf_listener = tf2_ros.TransformListener(tf_buffer)
+    tag_frame = rospy.get_param('~tag_frame')
     rate = rospy.Rate(tag_publish_rate)
     while not rospy.is_shutdown():
         rate.sleep()
